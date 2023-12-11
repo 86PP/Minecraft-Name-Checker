21f9538142f0c33c3ccfea88f15f0cc3fc0e443c
86PP <86835590+86PP@users.noreply.github.com>
2023-12-11 23:45:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests
-from colorama import init, Fore
+from colorama import Fore, Style, init
 import time
+from datetime import datetime
+
 
 init()
 
 ps_to_check = input("Place of the texte file to check :")
-ps_2 = ''.join(ps_to_check.split("'"))
+ps_2 = ''.join(ps_to_check.split('"'))
+ps_3 = ''.join(ps_2.split("'"))
 
 def test_pseudo(username):
     response = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{username}")
     return response.status_code == 200
 
-with open(f"{ps_2}", "r") as file:
+with open(f"{ps_3}", "r") as file:
     usernames = file.read().splitlines()
 
 total_usernames = len(usernames)
@@ -20,28 +23,29 @@ usernames = [username for username in usernames if len(username) >= 3]
 
 filtered_usernames = len(usernames)
 time_to_f = (total_usernames*2)/60
+time_to_f_rounded = time_to_f.__round__(1)
 
 print(f"Number total of names : {total_usernames}")
 print(f"Filter with less than 3 characters : {total_usernames - filtered_usernames}")
-print(f"The pseudo check was finis in {time_to_f} minutes")
+print(f"The pseudo check was finis in {time_to_f_rounded} minutes")
 
 
 available_usernames = []
 
 for index, username in enumerate(usernames, 1):
     result = test_pseudo(username)
-    times = time.strftime("%H:%M:%S", time.localtime())
-    status = f"[{times}] [No Available]" if result else f"[{times}] [Available]"
-    color = Fore.GREEN if result else Fore.RED
-    print(f"{index}/{filtered_usernames} - {username}: {color}{status}{Fore.RESET}")
+    now = datetime.now()
+    times = now.strftime(f"{Fore.BLUE}%H{Style.RESET_ALL}{Fore.BLACK}:{Style.RESET_ALL}{Fore.BLUE}%M{Style.RESET_ALL}{Fore.BLACK}:{Style.RESET_ALL}{Fore.BLUE}%S{Style.RESET_ALL}")
+    status = f"{Fore.WHITE}[{Fore.RED}No Available{Fore.WHITE}]" if result else f"{Fore.WHITE}[{Fore.GREEN}Available{Fore.WHITE}]"
+    print(f"[{times}]{status}{Fore.RESET} {index}/{filtered_usernames} - {username}")
     
-    if result:
+    if not result:
         available_usernames.append(username)
         with open("dispo.txt", "a") as dispo_file:
             dispo_file.write(username + '\n')
 
     if index < filtered_usernames:
-        time.sleep(2)  
+        time.sleep(1.5)  
 
 print("Test de pseudonymes terminé.")
 
